0730fd49967d4621e4a21d781e217f0c1b311db1
christian <christian.coley84@gmail.com>
2023-03-20 23:23:20 +0100

    add/pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { BailDocComponent } from './bail-doc/bail-doc.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormDocComponent } from './form-doc/form-doc.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { DateLeft } from './pipe/dateLeft.pipe';
 
 @NgModule({
-  declarations: [AppComponent, BailDocComponent, FormDocComponent],
+  declarations: [AppComponent, BailDocComponent, FormDocComponent, DateLeft],
   imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent],
