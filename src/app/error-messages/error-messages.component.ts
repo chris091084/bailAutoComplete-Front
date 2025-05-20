409d3615c67272b505021c8dcb3fc53aac04ae3e
christian <christian.coley84@gmail.com>
2025-05-20 16:42:00 +0200

    update-angular17

--- src/app/error-messages/error-messages.component.ts
+++ src/app/error-messages/error-messages.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { AbstractControl } from '@angular/forms';
 
@@ -6,6 +7,7 @@ import { AbstractControl } from '@angular/forms';
   selector: 'app-error-messages',
   templateUrl: './error-messages.component.html',
   styleUrls: ['./error-messages.component.scss'],
+  imports: [CommonModule],
 })
 export class ErrorMessagesComponent {
   @Input() control: AbstractControl | null = null;
