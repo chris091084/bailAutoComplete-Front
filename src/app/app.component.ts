aae81f99ccfef6f0eac575911b14f5fde94b74df
christian <christian.coley84@gmail.com>
2025-05-20 16:28:02 +0200

    fix-after-update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
+import { FormDocComponent } from './form-doc/form-doc.component';
+
 @Component({
+  standalone: true,
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  imports: [FormDocComponent],
 })
 export class AppComponent {
   title = 'bailAutoComplet';
--- src/app/error-messages/error-messages.component.ts
+++ src/app/error-messages/error-messages.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { AbstractControl } from '@angular/forms';
 
 @Component({
+  standalone: true,
   selector: 'app-error-messages',
   templateUrl: './error-messages.component.html',
   styleUrls: ['./error-messages.component.scss'],
