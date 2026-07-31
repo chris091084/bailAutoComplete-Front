2d5daffcbe4ddb74d91a4f87d130c28bc9be780e
Christian <49199172+chris091084@users.noreply.github.com>
2026-07-31 17:15:33 +0200

    new back (#46)
    Co-authored-by: Christian COLEY <christian.coley@smile.com>

--- environments/environment.prod.ts
+++ environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://spiritual-python-christiancoley-621b8ed3.koyeb.app',
+  apiUrl: 'https://bailautocontainers5e6cf272-bailauto-backend.functions.fnc.fr-par.scw.cloud',
 };
